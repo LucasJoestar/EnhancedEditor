77061f4f6c27fe77d952375fdc85a7790dfe8f03
Hubert Grossin <hubert.grossin@gmail.com>
2021-12-02 17:38:04 +0100

    Fixed missing TryGetComponent method
    Before Unity 2019.2, the TryGetComponent method doesn't exist. We added that method using Component and GameObject extensions.

--- Scripts/Core/Extensions/ComponentExtensions.cs
+++ Scripts/Core/Extensions/ComponentExtensions.cs
@@ -10,26 +10,11 @@ using UnityEngine;
 namespace EnhancedEditor
 {
     /// <summary>
-    /// Contains multiple <see cref="GameObject"/>-related extension methods.
+    /// Contains multiple <see cref="Component"/>-related extension methods.
     /// </summary>
-	public static class GameObjectExtensions
+	public static class ComponentExtensions
     {
         #region Content
-        /// <summary>
-        /// Adds a specific component to a <see cref="GameObject"/>
-        /// if none of that type is already attached to it, and return it.
-        /// </summary>
-        /// <typeparam name="T">Component type to add.</typeparam>
-        /// <param name="_gameObject"><see cref="GameObject"/> to add the component to.</param>
-        /// <returns>Newly added or already attached component to the <see cref="GameObject"/>.</returns>
-        public static T AddComponentIfNone<T>(this GameObject _gameObject) where T : Component
-        {
-            if (_gameObject.TryGetComponent(out T _component))
-                return _component;
-
-            return _gameObject.AddComponent<T>();
-        }
-
 #if !UNITY_2019_2_OR_NEWER
         /// <summary>
         /// Gets the component of the specified type, if it exists.<br/>
@@ -38,17 +23,17 @@ namespace EnhancedEditor
         /// </summary>
         /// <param name="_component">The output argument that will contain the component or null.</param>
         /// <returns>Returns true if the component is found, false otherwise.</returns>
-        public static bool TryGetComponent<T>(this GameObject _gameObject, out T _component) where T : Component
+        public static bool TryGetComponent<T>(this Component _source, out T _component) where T : Component
         {
-            _component = _gameObject.GetComponent<T>();
+            _component = _source.GetComponent<T>();
             return _component != null;
         }
 
         /// <inheritdoc cref="TryGetComponent{T}(GameObject, out T)"/>
         /// <param name="type">The type of the component to retrieve.</param>
-        public static bool TryGetComponent(this GameObject _gameObject, Type type, out Component _component)
+        public static bool TryGetComponent(this Component _source, Type type, out Component _component)
         {
-            _component = _gameObject.GetComponent(type);
+            _component = _source.GetComponent(type);
             return _component != null;
         }
 #endif
